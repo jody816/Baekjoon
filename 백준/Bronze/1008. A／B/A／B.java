d8b878614e3326ef29bd73aedb5a9f732af8858c
jody816 <81346079+jody816@users.noreply.github.com>
2023-06-24 18:17:02 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        double a = stdIn.nextInt(), b = stdIn.nextInt();
+
+        System.out.print(a/b);
+    }
+}
