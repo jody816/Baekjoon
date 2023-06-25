768f45605f85838897e211358513d1addaa55689
jody816 <81346079+jody816@users.noreply.github.com>
2023-06-25 16:59:14 +0900

    [Bronze V] Title: A+B - 4, Time: 224 ms, Memory: 17844 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        while (stdIn.hasNextInt()) {
+            int a = stdIn.nextInt();
+            int b = stdIn.nextInt();
+
+            System.out.println(a+b);
+        }
+    }
+}
