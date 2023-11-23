6a56c2411fa3de9efa65e02b7c8eee3a13aa1174
jody816 <81346079+jody816@users.noreply.github.com>
2023-11-23 20:41:30 +0900

    [Bronze V] Title: A+B - 2, Time: 136 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(n+m));
+        bw.flush();
+        bw.close();
+    }
+}
