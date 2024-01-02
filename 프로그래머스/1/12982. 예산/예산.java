6d730bbbae4ca75ee99d95364b31b3fd2f4a056e
jody816 <81346079+jody816@users.noreply.github.com>
2024-01-03 01:38:37 +0900

    [level 1] Title: 예산, Time: 0.32 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+
+       int sum = 0;
+       int count = 0;
+
+       for (int i = 0; i < d.length; i++) {
+         sum += d[i];
+         count++;
+         if (sum > budget) {
+           return i;
+         }
+       }
+
+       return count;
+    }
+}
