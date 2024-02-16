c1c401b664b0e519e0a413b99e5716d5cecfbbe3
jody816 <81346079+jody816@users.noreply.github.com>
2024-02-16 19:25:31 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 128 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(br.readLine());
+
+    bw.write(String.valueOf(factorial(n)));
+    bw.flush();
+    bw.close();
+  }
+
+  static long factorial(int n) {
+
+    if (n <= 1) {
+      return 1;
+    }
+
+    return n * factorial(n-1);
+  }
+}
