ed04ca77feac1d634f5a44cc201e5ec46c80509e
jody816 <81346079+jody816@users.noreply.github.com>
2024-02-18 20:12:59 +0900

    [Silver III] Title: 1로 만들기, Time: 252 ms, Memory: 67908 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,34 +1,45 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
 
-    static Integer[] dp;
-    static int recur(int n) {
+  static int n;
+  static Integer[] dp;
 
-        if (dp[n] == null)
-            if (n % 6 == 0)
-                dp[n] = Math.min(recur(n - 1), Math.min(recur(n / 3), recur(n / 2))) + 1;
-            else if (n % 3 == 0)
-                dp[n] = Math.min(recur(n / 3), recur(n - 1)) + 1;
-            else if (n % 2 == 0)
-                dp[n] = Math.min(recur(n / 2), recur(n - 1)) + 1;
-            else
-                dp[n] = recur(n - 1) + 1;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        return dp[n];
+    n = Integer.parseInt(br.readLine());
+    br.close();
+
+    dp = new Integer[n+1];
+    dp[0] = dp[1] = 0;
+
+    bw.write(String.valueOf(recur(n)));
+    bw.flush();
+    bw.close();
+  }
+
+  static int recur(int n) {
+
+    // 메모이제이션
+
+    // 탐색하지 않은 n일 경우
+    if (dp[n] == null) {
+
+      // 6 으로 나눠지는 경우
+      if (n%6 == 0) {
+        dp[n] = Math.min(recur(n/3), Math.min(recur(n/2), recur(n-1))) + 1;
+      } else if (n%3 == 0) {
+        dp[n] = Math.min(recur(n/3), recur(n-1)) + 1;
+      } else if (n%2 == 0) {
+        dp[n] = Math.min(recur(n/2), recur(n-1)) + 1;
+      } else {
+        dp[n] = recur(n-1) + 1;
+      }
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int n = Integer.parseInt(br.readLine());
-
-        dp = new Integer[n + 1];
-        dp[0] = dp[1] = 0;
-
-        bw.write(String.valueOf(recur(n)));
-        bw.flush();
-        bw.close();
-    }
+    return dp[n];
+  }
 }
