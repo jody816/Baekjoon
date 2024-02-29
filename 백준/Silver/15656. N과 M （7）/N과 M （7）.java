fc38254266f9ec5dfb7b384f8fe51f631108f953
jody816 <81346079+jody816@users.noreply.github.com>
2024-02-29 20:25:26 +0900

    [Silver III] Title: N과 M (7), Time: 660 ms, Memory: 123892 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  static int n, m;
+  static int[] arr;
+  static int[] check;
+  //static boolean[] visit;
+
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+
+    arr = Arrays.stream(br.readLine().split(" "))
+        .mapToInt(Integer::parseInt).toArray();
+
+    Arrays.sort(arr);
+
+    check = new int[m];
+    //visit = new boolean[n];
+
+    dfs(0);
+
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
+  static void dfs(int depth) {
+    if (depth == m) {
+      for (int i : check) {
+        sb.append(i).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+
+    for (int i = 0; i < n; i++) {
+      check[depth] = arr[i];
+      dfs(depth+1);
+    }
+  }
+}
