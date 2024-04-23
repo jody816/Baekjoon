e896f702838b1b89f8f7ffc371c9c1f56c6658d3
jody816 <81346079+jody816@users.noreply.github.com>
2024-04-23 18:33:26 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -2,32 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
   static int n;
-  static int[] stair;
-  static Integer[] dp;
-
+  static int[] arr;
+  static int[] dp;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     n = Integer.parseInt(br.readLine());
-    stair = new int[n+1];
+    arr = new int[n+1];
+
     for (int i = 1; i <= n; i++) {
-      stair[i] = Integer.parseInt(br.readLine());
+      arr[i] = Integer.parseInt(br.readLine());
     }
 
-    dp = new Integer[n+1];
-    dp[0] = stair[0];
-    dp[1] = stair[1];
+    dp = new int[n+1];
 
-    if (n >= 2) {
-      dp[2] = stair[1] + stair[2];
+    dp[1] = arr[1];
+
+    if (n > 1) {
+      dp[2] = arr[1] + arr[2];
     }
+
+//    if (n > 2) {
+//      dp[3] = Math.max(dp[1], dp[2]) + arr[3];
+//    }   이거 있으니까 안된다;
 
     for (int i = 3; i <= n; i++) {
-      dp[i] = Math.max(dp[i-2], dp[i-3] + stair[i-1]) + stair[i];
+      dp[i] = arr[i] + Math.max(dp[i-2], dp[i-3] + arr[i-1]);
     }
+    
     bw.write(String.valueOf(dp[n]));
     bw.flush();
     bw.close();
