8425450508da001a736d260c2ea0285d3109f7d8
jody816 <81346079+jody816@users.noreply.github.com>
2024-05-02 14:04:32 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 24.13 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,39 +1,39 @@
+import java.util.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 class Solution {
+    
+    static Map<String, Integer> map = new HashMap<>();
+    static StringBuilder sb = new StringBuilder();
+    
     public int[] solution(String today, String[] terms, String[] privacies) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
-        LocalDate date = LocalDate.parse(today, formatter);
 
-        int[] result = new int[privacies.length];
+      for (String i : terms) {
+        String[] term = i.split(" ");
 
-        int count = 0;
+        map.put(term[0], Integer.parseInt(term[1]));
+      }
 
-        
-        for (int i = 0; i < privacies.length; i++) {
-            String[] p = privacies[i].split(" ");
-            LocalDate pDate = LocalDate.parse(p[0], formatter);
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+      LocalDate Today = LocalDate.parse(today, formatter);
 
-            for (String term : terms) {
-                String[] t = term.split(" ");
+      System.out.println("Today = " + Today);
 
-                if (p[1].equals(t[0])) {
-                    if (date.isBefore(pDate.plusMonths(Integer.parseInt(t[1])))) {
-                        result[i] = i + 1;
-                        count++;
-                        break;
-                    }
-                }
-            }
+      for (int i = 0; i < privacies.length; i++) {
+
+        String[] privacy = privacies[i].split(" ");
+
+        LocalDate date = LocalDate.parse(privacy[0], formatter).plusMonths(map.get(privacy[1])).minusDays(1);
+
+        System.out.println("date = " + date);
+
+        if (date.isBefore(Today)) {
+          sb.append(i+1).append(" ");
         }
+      }
 
-        int[] r = new int[privacies.length-count];
-        int c = 0;
-        for (int i = 0; i < result.length; i++) {
-            if (result[i] == 0)
-                r[c++] = i+1;
-        }
-        return r;
+      return Arrays.stream(sb.toString().split(" "))
+          .mapToInt(Integer::parseInt).toArray();
     }
 }
