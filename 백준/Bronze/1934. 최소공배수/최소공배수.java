dee21d3fabaa360ceb06323db7838d63dfa553a5
jody816 <81346079+jody816@users.noreply.github.com>
2024-09-26 14:05:12 +0900

    [Bronze I] Title: 최소공배수, Time: 140 ms, Memory: 16080 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+
+            String[] input = br.readLine().split(" ");
+
+            int first = Integer.parseInt(input[0]);
+            int second = Integer.parseInt(input[1]);
+
+            bw.write(lcm(first, second) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
