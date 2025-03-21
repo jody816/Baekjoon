eddc6992bc64f0a0b131c2a81f586ead572cdcaf
jody816 <81346079+jody816@users.noreply.github.com>
2025-03-21 14:42:51 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,28 @@
 import java.util.*;
 
 class Solution {
-
-  static boolean[] visit;
-
-  public int solution(int n, int[][] computers) {
-
-    visit = new boolean[n];
-
-    int count = 0;
-
-    for (int i = 0; i < n; i++) {
-      if (!visit[i]) {
-        count++;
-        dfs(computers, i);
-      }
+    
+    static boolean[] visit;
+    public int solution(int n, int[][] computers) {
+        int count = 0;
+        visit = new boolean[n];
+        
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                dfs(computers, i);
+                count++;
+            }
+        }
+        
+        return count;
     }
-
-    return count;
-  }
-
-  static void dfs(int[][] computers, int node) {
-
-    visit[node] = true;
-
-    for (int i = 0; i < computers[node].length; i++) {
-      if (computers[node][i] == 1 && !visit[i]) {
-        dfs(computers, i);
-      }
+    
+    static void dfs(int[][] computers, int node) {
+        visit[node] = true;
+        for (int i = 0; i < computers[node].length; i++) {
+            if (!visit[i] && computers[node][i] == 1) {
+                dfs(computers, i);
+            }
+        }
     }
-  }
 }
