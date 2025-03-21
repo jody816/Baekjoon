8ef81efc6848136b003670a2b52d286ddc6185dd
jody816 <81346079+jody816@users.noreply.github.com>
2025-03-21 15:06:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.22 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,48 +1,54 @@
 import java.util.*;
 
 class Solution {
-    static int[][] map;
     static boolean[][] visit;
     static int[] x = {1, -1, 0, 0};
-    static int[] y = {0, 0, -1, 1};
+    static int[] y = {0, 0, 1, -1};
+    static int result = 0;
     public int solution(int[][] maps) {
-        map = new int[maps.length][maps[0].length];
+        
         visit = new boolean[maps.length][maps[0].length];
         
-        bfs(maps);
-        
-        int result = -1;
-        
-        if (map[maps.length-1][maps[0].length-1] > 0) {
-            result = map[maps.length-1][maps[0].length-1];
+        for (int i = 0; i < maps.length; i++) {
+            for (int j = 0; j < maps[0].length; j++) {
+                if (maps[i][j] == 0) {
+                    visit[i][j] = true;
+                }
+            }
         }
+        
+        bfs(maps);
         
         return result;
     }
     
     static void bfs(int[][] maps) {
-        Queue<int[]> q = new LinkedList<>();
+        Queue<int[]> queue = new LinkedList<>();
         
-        q.offer(new int[]{0, 0});
+        queue.offer(new int[]{0, 0});
         visit[0][0] = true;
-        map[0][0] = 1;
-        while (!q.isEmpty()) {
-            int[] start = q.poll();
+        
+        while (!queue.isEmpty()) {
             
-            if (start[0] == map.length-1 && start[1] == map[0].length-1) {
+            int[] q = queue.poll();
+            
+            if (q[0] == maps.length-1 && q[1] == maps[0].length-1) {
+                result = maps[q[0]][q[1]];
                 return;
             }
             
             for (int i = 0; i < 4; i++) {
-                int newX = start[0] + x[i];
-                int newY = start[1] + y[i];
+                int X = q[0] + x[i];
+                int Y = q[1] + y[i];
                 
-                if (newX >= 0 && newX < map.length && newY >= 0 && newY < map[0].length && !visit[newX][newY] && maps[newX][newY] == 1) {
-                    q.offer(new int[]{newX, newY});
-                    visit[newX][newY] = true;
-                    map[newX][newY] = map[start[0]][start[1]] + 1;
+                if (X >= 0 && Y >= 0 && X < maps.length && Y < maps[0].length && maps[X][Y] > 0 && !visit[X][Y]) {
+                    visit[X][Y] = true;
+                    queue.offer(new int[]{X, Y});
+                    maps[X][Y] += maps[q[0]][q[1]];
                 }
             }
         }
+        
+        result = -1;
     }
 }
