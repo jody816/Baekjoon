55024a97782fbfa11cb47a054810712aeab39840
jody816 <81346079+jody816@users.noreply.github.com>
2025-03-24 14:43:53 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 26.21 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,34 +2,28 @@ import java.util.*;
 
 class Solution {
     
-    static Queue<Integer> bri = new LinkedList<>();
     static int contain = 0;
+    static Queue<Integer> q = new LinkedList<>();
     
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
         
-        for(int t : truck_weights) {
-			
-            while(true) {
-                //큐가 비어있다면 다음 트럭 삽입
-                //큐가 비어있지 않을 때
-                //큐의 사이즈와 다리의 길이가 같다면 큐에서 큐에서 처음 값을 빼고 무게 합 -
-                //다음 트럭이 최대 무게 초과
-                //다음 트럭이 최대 무게 이내
-
-                if (bri.isEmpty()) {
-                    bri.offer(t);
+        for (int t : truck_weights) {
+            
+            while (true) {
+                if (q.isEmpty()) {
+                    q.offer(t);
                     contain += t;
                     answer++;
                     break;
                 } else {
-                    if (bri.size() == bridge_length) {
-                        contain -= bri.poll();
+                    if (q.size() == bridge_length) {
+                        contain -= q.poll();
                     } else if (contain + t > weight) {
-                        bri.offer(0);
+                        q.offer(0);
                         answer++;
                     } else if (contain + t <= weight) {
-                        bri.offer(t);
+                        q.offer(t);
                         answer++;
                         contain += t;
                         break;
