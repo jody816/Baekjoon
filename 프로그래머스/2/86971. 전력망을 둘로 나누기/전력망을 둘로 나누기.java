e4c93b0a8abd90cef4d16f93283650d50f94ffc2
jody816 <81346079+jody816@users.noreply.github.com>
2025-03-25 13:07:38 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.09 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,49 +1,51 @@
 import java.util.*;
 
 class Solution {
-    
     static List<List<Integer>> tree = new ArrayList<>();
     static int answer = Integer.MAX_VALUE;
     
     public int solution(int n, int[][] wires) {
-
+        
         for (int i = 0; i <= n; i++) {
             tree.add(new ArrayList<>());
         }
-
-        for (int[] wire : wires) {
-            tree.get(wire[0]).add(wire[1]);
-            tree.get(wire[1]).add(wire[0]);
+        
+        for (int[] arr : wires) {
+            tree.get(arr[0]).add(arr[1]);
+            tree.get(arr[1]).add(arr[0]);
         }
-
-        for (int i = 0; i < wires.length; i++) {
-
+        
+        for (int[] arr : wires) {
             boolean[] visit = new boolean[n+1];
-
-            tree.get(wires[i][0]).remove(Integer.valueOf(wires[i][1]));
-            tree.get(wires[i][1]).remove(Integer.valueOf(wires[i][0]));
-
+            
+            tree.get(arr[0]).remove(Integer.valueOf(arr[1]));
+            tree.get(arr[1]).remove(Integer.valueOf(arr[0]));
+            
             int count = dfs(1, visit);
-
-            answer = Math.min(answer, Math.abs(count - (n - count)));
-
-            tree.get(wires[i][0]).add(wires[i][1]);
-            tree.get(wires[i][1]).add(wires[i][0]);
+            
+            answer = Math.min(answer, Math.abs((n-count) - count));
+            
+            tree.get(arr[0]).add(arr[1]);
+            tree.get(arr[1]).add(arr[0]);
         }
-
+        
         return answer;
     }
-
-    private static int dfs(int start, boolean[] visit) {
-        visit[start] = true;
-        int cnt = 1;
-
-        for (int nextVertex : tree.get(start)) {
-            if (!visit[nextVertex]) {
-                cnt += dfs(nextVertex, visit);
+    
+    static private int dfs(int wire, boolean[] visit) {
+        
+        visit[wire] = true;
+        int c = 1;
+        
+        for (int i = 0; i < tree.get(wire).size(); i++) {
+            
+            int nextWire = tree.get(wire).get(i);
+            
+            if (!visit[nextWire]) {
+                c += dfs(nextWire, visit);
             }
         }
-
-        return cnt;
+        
+        return c;
     }
 }
